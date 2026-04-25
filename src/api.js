b4ca086cf749c0a564c5bb2a464611a1085c023b
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2026-04-25 10:52:51 -0500

    Mejorando el api.js

--- src/api.js
+++ src/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Tu URL de Render (asegúrate de incluir el /api/ al final)
-  baseURL: 'https://back-e-commerce-h5xg.onrender.com/api/',
+  // Importante: import.meta.env.VITE_API_URL lee la "Key" de Vercel
+  // Si no la encuentra (como en local), usa la de Render directamente
+  baseURL: import.meta.env.VITE_API_URL || 'https://back-e-commerce-h5xg.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
